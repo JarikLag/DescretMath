689b5a23568b5727d53e831e9ecd095fe66d0efa
JarikLag <jarikdorohov@gmail.com>
2019-05-21 18:29:01 +0300

    fixed last lab

--- ADS/2-й курс/(6) Математика и криптография/(D)Китайская теорема.cpp
+++ ADS/2-й курс/(6) Математика и криптография/(D)Китайская теорема.cpp
@@ -25,50 +25,34 @@ typedef long long LL;
 const LL LONG_INF = 1e18;
 const int INT_INF = 1e9 + 7;
 
-LL gcd(LL a, LL b, LL& x, LL& y) {
-	if (a == 0) {
-		x = 0; y = 1;
-		return b;
-	}
-	LL x1, y1;
-	LL d = gcd(b % a, a, x1, y1);
-	x = y1 - (b / a) * x1;
-	y = x1;
-	return d;
-}
-
 LL inversed(LL a, LL m) {
-	LL x, y;
-	LL g = gcd(a, m, x, y);
-	if (g != 1) {
-		assert("SHIT");
-	}
-	else {
-		x = (x % m + m) % m;
-		return x;
-	}
+	a = a % m;
+	for (LL x = 1; x < m; x++)
+		if ((a * x) % m == 1)
+			return x;
 }
 
-LL chineseTheorem(vector<int> a, vector<int> p) {
-	vector<vector<int>> r(2, vector<int>(2, 0));
-	for (int i = 0; i < 2; ++i) {
-		for (int j = i + 1; j < 2; ++j) {
+LL chineseTheorem(vector<LL> a, vector<LL> p, int size) {
+	vector<vector<LL>> r(size, vector<LL>(size, 0));
+	for (int i = 0; i < size; ++i) {
+		for (int j = i + 1; j < size; ++j) {
 			r[i][j] = inversed(p[i], p[j]);
 		}
 	}
-	LL result = 0, mult = 1;
-	vector<int> x(2, 0);
+	LL result = 0;
+	LL mult_p = 1;
+	vector<LL> x(size);
 	for (int i = 0; i < 2; ++i) {
 		x[i] = a[i];
 		for (int j = 0; j < i; ++j) {
-			x[i] = r[j][i] * (x[i] - x[j]);
+			x[i] = (x[i] - x[j]) * r[j][i];
 			x[i] = x[i] % p[i];
 			if (x[i] < 0) {
 				x[i] += p[i];
 			}
 		}
-		result += mult * x[i];
-		mult *= p[i];
+		result += mult_p * x[i];
+		mult_p *= p[i];
 	}
 	return result;
 }
@@ -81,8 +65,8 @@ int main() {
 	/*freopen("assignment.in", "r", stdin);
 	freopen("assignment.out", "w", stdout);*/
 #endif
-	vector<int> a(2), p(2);
+	vector<LL> a(2), p(2);
 	cin >> a[0] >> a[1] >> p[0] >> p[1];
-	cout << chineseTheorem(a, p);
+	cout << chineseTheorem(a, p, 2);
 	return 0;
 }
