4b10346ec3aae312d38bb85c732677dd345a1181
Yaroslav Abubakirov <jarikdorohov@gmail.com>
2019-06-05 01:28:56 +0300

    fix 6th task

--- ADS/2-й курс/(6) Математика и криптография/(F)Задача для второклассника.cpp
+++ ADS/2-й курс/(6) Математика и криптография/(F)Задача для второклассника.cpp
@@ -1,4 +1,5 @@
 #pragma warning(disable : 4996)
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -22,53 +23,61 @@
 using namespace std;
 
 typedef long long LL;
+typedef complex<double> base;
 
 const LL LONG_INF = 1e18;
 const int INT_INF = 1e9 + 7;
 
-LL gcd(LL a, LL b, LL& x, LL& y) {
-	if (a == 0) {
-		x = 0; y = 1;
-		return b;
+void fft(vector<base>& a, bool invert) {
+	int n = a.size();
+	if (n == 1) {
+		return;
 	}
-	LL x1, y1;
-	LL d = gcd(b % a, a, x1, y1);
-	x = y1 - (b / a) * x1;
-	y = x1;
-	return d;
+	vector<base> a0(n / 2), a1(n / 2);
+	for (int i = 0, j = 0; i < n; i += 2, ++j) {
+		a0[j] = a[i];
+		a1[j] = a[i + 1];
 	}
-
-LL inversed(LL a, LL m) {
-	LL x, y;
-	LL g = gcd(a, m, x, y);
-	if (g != 1) {
-		assert("SHIT");
-	}
-	else {
-		x = (x % m + m) % m;
-		return x;
+	fft(a0, invert);
+	fft(a1, invert);
+	double ang = 2 * M_PI / n * (invert ? -1 : 1);
+	base w(1), wn(cos(ang), sin(ang));
+	for (int i = 0; i < n / 2; ++i) {
+		a[i] = a0[i] + w * a1[i];
+		a[i + n / 2] = a0[i] - w * a1[i];
+		if (invert) {
+			a[i] /= 2, a[i + n / 2] /= 2;
+		}
+		w *= wn;
 	}
 }
 
-LL binaryMul(LL a, LL b, LL m) {
-	if (b == 1) {
-		return a;
+void multiply(const vector<int>& a, const vector<int>& b, vector<int>& res) {
+	vector<base> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+	int n = 1;
+	while (n < max(a.size(), b.size())) {
+		n <<= 1;
 	}
-	if (b % 2 == 0) {
-		LL t = binaryMul(a, b / 2, m);
-		return (2 * t) % m;
+	n <<= 1;
+	fa.resize(n), fb.resize(n);
+	fft(fa, false), fft(fb, false);
+	for (int i = 0; i < n; ++i) {
+		fa[i] *= fb[i];
 	}
-	return (binaryMul(a, b - 1, m) + a) % m;
+	fft(fa, true);
+	res.resize(n);
+	for (int i = 0; i < n; ++i) {
+		res[i] = int(fa[i].real() + 0.5);
+	}
 }
 
-LL binaryPow(LL a, LL b, LL m) {
-	if (b == 0)
-		return 1;
-	if (b % 2 == 0) {
-		LL t = binaryPow(a, b / 2, m);
-		return binaryMul(t, t, m) % m;
+void normalize(vector<int>& a) {
+	int carry = 0;
+	for (int i = 0; i < a.size(); ++i) {
+		a[i] += carry;
+		carry = a[i] / 10;
+		a[i] %= 10;
 	}
-	return (binaryMul(binaryPow(a, b - 1, m), a, m)) % m;
 }
 
 int main() {
@@ -79,17 +88,45 @@ int main() {
 	/*freopen("assignment.in", "r", stdin);
 	freopen("assignment.out", "w", stdout);*/
 #endif
-	int n, e, c;
-	cin >> n >> e >> c;
-	vector<int> divisors;
-	for (int i = 2; i <= sqrt(n); ++i) {
-		if (n % i == 0) {
-			divisors.push_back(i);
-			divisors.push_back(n / i);
-		}
+	string fst, snd;
+	cin >> fst >> snd;
+	bool negative = true, negF = false, negS = false;
+	if (fst[0] == '-') {
+		negF = true;
+		fst[0] = '0';
 	}
-	int mod = (divisors[0] - 1) * (divisors[1] - 1);
-	int d = inversed(e, mod);
-	cout << binaryPow(c, d, n);
+	if (snd[0] == '-') {
+		negS = true;
+		snd[0] = '0';
+	}	
+	if ((negF && negS) || (!negF && !negS)) {
+		negative = false;
+	}
+	vector<int> a(fst.size()), b(snd.size());
+	for (int i = 0; i < a.size(); ++i) {
+		a[i] = fst[i] - '0';
+	}
+	for (int i = 0; i < b.size(); ++i) {
+		b[i] = snd[i] - '0';
+	}
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+	vector<int> res;
+	multiply(a, b, res);
+	normalize(res);
+	int ind = res.size() - 1;
+	while (ind >= 0 && res[ind] == 0) {
+		--ind;
+	}
+	if (ind == -1) {
+		cout << 0;
+		return 0;
+	}
+	if (negative) {
+		cout << "-";
+	}
+	for (; ind >= 0; --ind) {
+		cout << res[ind];
+	}
 	return 0;
 }
